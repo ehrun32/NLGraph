08c9745111a856086f1d74b951560dfee34815bd
Arun Cheriakara <57046416+ehrun32@users.noreply.github.com>
2025-04-04 17:41:15 -0400

    more testing

--- check_output.py
+++
@@ -1,7 +0,0 @@
-# check_output.py
-import numpy as np
-
-path = "log/cycle/gpt-3.5-turbo-medium-20250404---17-29-CoT+SC/answer.npy"
-answer = np.load(path, allow_pickle=True)
-print("Answer:", answer)
-
